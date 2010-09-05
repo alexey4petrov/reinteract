80a3f9019678c99317afcf5563dc30fbc7b3b1ef
Owen W. Taylor <otaylor@fishsoup.net>
2010-09-05 12:38:10 -0400

    Basic worksheet printing support
    Fill in WorksheetPrintOperation skeleton. Printing matches ShellBuffer
    display closely. Not implemented:
    
     - Printing of custom results
     - Page numbers
     - Breaking chunks between multiple pages

--- lib/reinteract/worksheet_print.py
+++ lib/reinteract/worksheet_print.py
@@ -8,10 +8,222 @@
 
 import gtk
 import logging
+import pango
+import pangocairo
+
+from chunks import StatementChunk, CommentChunk, BlankChunk
+from cStringIO import StringIO
+import doc_format
+from statement import Statement, WarningResult, HelpResult, CustomResult
+from style import DEFAULT_STYLE
 
 _debug = logging.getLogger("PrintOperation").debug
 
+# The two phases of printing
+_MEASURE = 0
+_RENDER = 1
+
+class _Page(object):
+    def __init__(self, start_line, end_line):
+        self.start_line = start_line
+        self.end_line = end_line
+
+class _PageLayout(object):
+    # This object  does the actual work of page layout for a worksheet; it keeps
+    # of the state of the page layout, including things like the vertical position on the
+    # page. During the _MEASURE phase, one _PageLayout object is used for the entire
+    # worksheet. During rendering, we create a separate one per page. There's some
+    # confusion with pango.Layout in the name; pango.Layout's are used for individual
+    # paragraphs of text.
+
+    def __init__(self, worksheet, context, phase):
+        self.worksheet = worksheet
+        self.context = context
+
+        self.left_margin = 0.5 / 72.
+        self.right_margin = 0.5 / 72.
+        self.top_margin = 0.5 / 72.
+        self.bottom_margin = 1 / 72.
+        self.font = pango.FontDescription("monospace 12")
+
+        self.phase = phase
+
+        if self.phase == _MEASURE:
+            self.pages = []
+        else: # phase == _RENDER
+            self.cr = self.context.get_cairo_context()
+
+        self.y = self.top_margin
+        self.page_start_line = None
+
+    def create_pango_layout(self, text, *styles):
+        layout = self.context.create_pango_layout()
+
+        layout.set_text(text)
+        layout.set_font_description(self.font)
+
+        # We need to save some stuff for when we render; just stick extra properties on the
+        # layout. We may want to move to a setup where we have "print objects" for layouts
+        # or custom results.
+        layout._left_margin = 0
+        layout._right_margin = 0
+        layout._paragraph_background = None
+
+        attrs = pango.AttrList()
+        for style in styles:
+            spec = DEFAULT_STYLE.get_spec(style)
+            spec.add_pango_attributes(attrs, start_index=0, end_index=0x7fffffff)
+
+            # We are fudging pixels vs. points here
+            if spec.left_margin:
+                layout._left_margin = spec.left_margin
+            if spec.left_margin:
+                layout._right_margin = spec.right_margin
+            if spec.paragraph_background:
+                layout._paragraph_background = pango.Color(spec.paragraph_background)
+
+        layout._width = self.context.get_width() - self.left_margin - layout._left_margin - self.right_margin - layout._right_margin
+        layout.set_width(int(pango.SCALE * layout._width))
+
+        layout.set_attributes(attrs)
+
+        return layout
+
+    def append_pango_layout(self, layout):
+        _, layout_height = layout.get_size()
+        layout_height = layout_height / pango.SCALE
+
+        if self.phase == _MEASURE:
+            # BlankChunks never force page breaks, otherwise, if we've overflowed, and this
+            # chunk isn't already on a page of its own, start a new page
+            if (not isinstance(self.current_chunk, BlankChunk) and
+                layout_height + self.y > self.context.get_height() - self.bottom_margin and
+                self.page_start_line != self.current_chunk.start):
+                self.pages.append(_Page(self.page_start_line, self.current_chunk.start))
+
+                self.y = self.y - self.chunk_start_y + self.top_margin
+                self.chunk_start_y = self.top_margin
+                self.page_start_line = self.current_chunk.start
+        else: # phase == _RENDER
+            if layout._paragraph_background is not None:
+                self.cr.save()
+                self.cr.set_source_rgb(layout._paragraph_background.red / 65535.,
+                                       layout._paragraph_background.green / 65535.,
+                                       layout._paragraph_background.blue / 65535.)
+                self.cr.rectangle(self.left_margin + layout._left_margin,
+                                  self.y,
+                                  layout._width,
+                                  layout_height)
+                self.cr.fill()
+                self.cr.restore()
+
+            self.cr.move_to(self.left_margin + layout._left_margin, self.y)
+            self.cr.show_layout(layout)
+
+        self.y += layout_height
+
+    def append_chunk_text(self, chunk):
+        text = self.worksheet.get_text(start_line=chunk.start, end_line=chunk.end - 1)
+
+        if isinstance(chunk, CommentChunk):
+            layout = self.create_pango_layout(text, 'comment')
+        else:
+            layout = self.create_pango_layout(text)
+
+        if isinstance(chunk, StatementChunk):
+            attrs = layout.get_attributes() # makes a copy
+            index = 0
+
+            # The complexity here is because Pango attributes encode positions by byte
+            # index for UTF-8 encoded text while we store the tokenization in Unicode
+            # character positions.
+            for i in xrange(chunk.start, chunk.end):
+                line = self.worksheet.get_line(i)
+                offset = 0
+                for token_type, start_offset, end_offset, _ in chunk.tokenized.get_tokens(i - chunk.start):
+                    start_index = index + len(line[offset:start_offset].encode("UTF-8"))
+                    end_index = start_index + len(line[start_offset:end_offset].encode("UTF-8"))
+                    spec = DEFAULT_STYLE.get_spec(token_type)
+                    if spec is not None:
+                        spec.add_pango_attributes(attrs, start_index, end_index)
+                    index = end_index
+                    offset = end_offset
+                index += 1 # newline
+
+            layout.set_attributes(attrs) # set the copy back
+
+        self.append_pango_layout(layout)
+
+    def append_chunk_results(self, chunk):
+        if not isinstance(chunk, StatementChunk):
+            return
+
+        def create_result_layout(text, style=None):
+            styles = ['result']
+            if style is not None:
+                styles.append(style)
+            if chunk.needs_execute:
+                styles.append('recompute')
+            return self.create_pango_layout(text, *styles)
+
+        if chunk.error_message:
+            layout = create_result_layout(chunk.error_message)
+            self.append_pango_layout(layout)
+        elif chunk.results is not None:
+            styles = ['result']
+            for result in chunk.results:
+                if isinstance(result, basestring):
+                    layout = create_result_layout(result)
+                    self.append_pango_layout(layout)
+                elif isinstance(result, WarningResult):
+                    layout = create_result_layout(result.message, 'warning')
+                    self.append_pango_layout(layout)
+                elif isinstance(result, HelpResult):
+                    si = StringIO()
+                    attrs = pango.AttrList()
+
+                    index = [0] # array so we can mutate within nested function
+                    def callback(text, bold):
+                        if isinstance(text, unicode):
+                            text = text.encode("UTF-8")
+                        si.write(text)
+                        end_index = index[0] + len(text)
+                        if bold:
+                            attrs.insert(pango.AttrWeight(pango.WEIGHT_BOLD, index[0], end_index))
+                        index[0] = end_index
+
+                    doc_format.format_docs(result.arg, callback)
+                    layout = create_result_layout(si.getvalue(), 'help')
+                    paragraph_attrs = layout.get_attributes()
+                    paragraph_attrs.splice(attrs, 0, index[0])
+                    layout.set_attributes(paragraph_attrs)
+                    self.append_pango_layout(layout)
+                elif isinstance(result, CustomResult):
+                    # FIXME: implement
+                    pass
+
+    def append_chunk(self, chunk):
+        if self.page_start_line is None:
+            self.page_start_line = chunk.start
+
+        self.current_chunk = chunk
+        self.chunk_start_y = self.y
+
+        self.append_chunk_text(chunk)
+        self.append_chunk_results(chunk)
+
+        self.current_chunk = None
+        self.page_end_line = chunk.end
+
+    def finish(self):
+        if self.phase == _MEASURE and self.page_start_line is not None:
+            self.pages.append(_Page(self.page_start_line, self.page_end_line))
+
 class WorksheetPrintOperation(gtk.PrintOperation):
+    """
+    Subclass of gtk.PrintOperation to print a worksheet.
+    """
+
     __gsignals__ = {
         'begin-print' : 'override',
         'draw-page': 'override'
@@ -20,8 +232,27 @@ class WorksheetPrintOperation(gtk.PrintOperation):
     def __init__(self, worksheet):
         gtk.PrintOperation.__init__(self)
 
+        self.worksheet = worksheet
+
+        self.set_unit(gtk.UNIT_POINTS)
+
     def do_begin_print(self, context):
-        self.set_n_pages(1)
+        page_layout = _PageLayout(self.worksheet, context, phase=_MEASURE)
+
+        for chunk in self.worksheet.iterate_chunks():
+            page_layout.append_chunk(chunk)
+
+        page_layout.finish()
+
+        self.__pages = page_layout.pages
+        self.set_n_pages(len(self.__pages))
 
     def do_draw_page(self, context, page_nr):
-        pass
+        page = self.__pages[page_nr]
+
+        page_layout = _PageLayout(self.worksheet, context, phase=_RENDER)
+
+        for chunk in self.worksheet.iterate_chunks(page.start_line, page.end_line):
+           page_layout.append_chunk(chunk)
+
+        page_layout.finish()
