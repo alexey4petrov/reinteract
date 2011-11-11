a282f4fa8c40603462d73fddcedaf6a805e063f9
Robert Schroll <rschroll@gmail.com>
2011-11-11 17:30:47 -0500

    Use a custom documentation formatter for shorter module docs
    The documentation formatter from pydoc includes in module documentation
    the full documentation of each class and function.  This takes a while
    to produce for large modules (e.g. gtk).  Here, we add a new formatter
    that speeds up this process in two ways:
    1) Calls to document() from within docmodule() are short-circuited.
    2) Some sections of the docs are not included in the full docs.  Their
    contents are better explored through introspection.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=659335

--- lib/reinteract/doc_format.py
+++ lib/reinteract/doc_format.py
@@ -15,6 +15,45 @@ from data_format import insert_with_tag, is_data_object
 BOLD_RE = re.compile("(?:(.)\b(.))+")
 STRIP_BOLD_RE = re.compile("(.)\b(.)")
 
+class TextDocShort(pydoc.TextDoc):
+    """Formatter class that produces shorter docs for modules.
+
+    The document() method is short-circuited when producing docs for a
+    module.  This keeps it from examining the classes and functions in
+    the module.  Additionally, the section() method returns a blank
+    string for several sections of the module docs which contain objects
+    better discovered through introspection.
+
+    """
+
+    def __init__(self, *args, **kw):
+        self._indocmodule = False
+
+    def docmodule(self, *args, **kw):
+        self._indocmodule = True
+        try:
+            res = pydoc.TextDoc.docmodule(self, *args, **kw)
+        finally:
+            self._indocmodule = False
+        return res
+
+    def document(self, *args, **kw):
+        if self._indocmodule:
+            return ''
+        return pydoc.TextDoc.document(self, *args, **kw)
+
+    def section(self, title, contents):
+        if title in ('PACKAGE CONTENTS', 'SUBMODULES', 'CLASSES', 'FUNCTIONS', 'DATA'):
+            return ''
+        if not pydoc.strip(contents):
+            # This should only happen for 'CLASSES' or 'FUNCTIONS', but
+            # we'll be careful anyway.
+            return ''
+        return pydoc.TextDoc.section(self, title, contents)
+
+textdocshort = TextDocShort()
+
+
 def format_docs(obj, callback):
     """Gets the documentation for a given object, and format it simply
       with a distinction between bold and normal
@@ -31,7 +70,7 @@ def format_docs(obj, callback):
         obj = type(obj)
         
     name = getattr(obj, '__name__', None)
-    document = pydoc.text.document(obj, name)
+    document = textdocshort.document(obj, name)
 
     # pydoc.text.document represents boldface with overstrikes, we need to
     # reverse engineer this and find the spans of bold text
