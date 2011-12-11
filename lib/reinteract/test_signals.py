9ce6adc52a408f3b53ad250f97e99b8d61c60bcc
Alexey Petrov <alexey.petrov.nnov@gmail.com>
2011-12-11 14:24:42 +0300

    advancing automatic tests

--- lib/reinteract/test_signals.py
+++ lib/reinteract/test_signals.py
@@ -15,7 +15,7 @@ Modified: January 08, 2011 ( multi-threading support )
 """
 
 #--------------------------------------------------------------------------------------
-def test_signals() :
+def test_signals_0() :
     #--------------------------------------------------------------------------------------
     from signals import Signal
 
@@ -88,9 +88,70 @@ def test_signals() :
 
 
 #--------------------------------------------------------------------------------------
+def test_signals_1() :
+    #--------------------------------------------------------------------------------------
+    from signals import Signal
+
+    #--------------------------------------------------------------------------------------
+    class Button:
+        def __init__(self):
+            # Creating a signal as a member of a class
+            self.sigClick = Signal()
+            pass
+        pass
+
+    #--------------------------------------------------------------------------------------
+    class Listener:
+        def __init__( self ):
+            self._log = []
+            pass
+        def onClick( self ) :
+            self._log.append( "onClick " )
+            pass
+        def __len__( self ) :
+            return len( self._log )
+        def clean( self ) :
+            del self._log[ : ]
+            pass
+        pass
+
+    #--------------------------------------------------------------------------------------
+    # a sample function to connect to the signal
+    def listenFunction():
+        print "listenFunction"
+   
+    #--------------------------------------------------------------------------------------
+    # a function that accepts arguments
+    def listenWithArgs(text):
+        print "listenWithArgs: ", text
+
+    #--------------------------------------------------------------------------------------
+    b = Button()
+    l = Listener()
+    
+    #--------------------------------------------------------------------------------------
+    # Demonstrating connecting and calling signals
+    b.sigClick.connect(l.onClick)
+
+    b.sigClick()
+    assert len( l ) == 1
+
+    #--------------------------------------------------------------------------------------
+    # Disconnecting all signals
+    b.sigClick.disconnectAll()
+    b.sigClick()
+
+    assert len( l ) == 1
+
+    #--------------------------------------------------------------------------------------
+    pass
+
+
+#--------------------------------------------------------------------------------------
 if __name__ == "__main__":
     #--------------------------------------------------------------------------------------
-    test_signals()
+    # test_signals_0()
+    test_signals_1()
 
     #--------------------------------------------------------------------------------------
     pass
